eea3d3896c5944f2c310af19f5cd612999a6cb0d
Cass <cassianoleaoramos@gmail.com>
2023-10-16 20:26:47 -0300

    updating to angular typed forms

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -9,27 +10,33 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  });
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
-  ) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null],
-    });
-  }
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {}
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      (result) => console.log(result),
+      (result) => this.onSuccess(),
       (error) => this.onError()
     );
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Course successfully added', '', { duration: 3000 });
+    this.onCancel();
+  }
 
   private onError() {
     this.snackBar.open('Error on saving course', '', { duration: 3000 });
