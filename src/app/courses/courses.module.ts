2b44fe9db3518ffb9f881fcc95100c59fbc09a00
Cass <cassianoleaoramos@gmail.com>
2023-12-31 18:57:45 -0300

    Migrate to standalone components - remove unnecessary modules

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,20 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './containers/courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { CourseFormComponent } from './containers/course-form/course-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 
 @NgModule({
     imports: [
-        CommonModule,
-        CoursesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        CoursesComponent, CourseFormComponent, CoursesListComponent,
-    ],
+    CommonModule,
+    CoursesRoutingModule,
+    ReactiveFormsModule,
+    CoursesComponent, CourseFormComponent, CoursesListComponent,
+],
 })
 export class CoursesModule {}
